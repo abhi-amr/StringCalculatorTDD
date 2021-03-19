02f4d1adda7cb94e5260bee7a55daf9d9cccd1e9
Abhishek Kumar <iamtonyriddle@outlook.com>
2021-03-19 19:33:14 +0530

    refactored

--- String Calculator/src/TestSamples.java
+++ String Calculator/src/TestSamples.java
@@ -2,11 +2,10 @@ import org.junit.jupiter.api.Test;
 //import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.*;
 class TestSamples{
+	private StringCalculator calc = new StringCalculator();
 	
 	@Test
 	void ForEmptyStringReturnZero() {
-		StringCalculator calc = new StringCalculator();
-		//assertThat(calc.Add("")).isEqualTo(0);
 		assertEquals(calc.Add(""), 0);
 	}
 }
